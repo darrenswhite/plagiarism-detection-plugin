809b80ca8b4c396d93e714f7d5e30351c674b3e8
Darren White <darrenswhite95@gmail.com>
2018-02-12 13:14:28 +0000

    Refactor copy-paste test

--- tests/com/daw48/detector/CopyPasteDetectionTest.java
+++ tests/com/daw48/detector/CopyPasteDetectionTest.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
 import java.util.Objects;
 
 /**
@@ -18,24 +17,25 @@ public class CopyPasteDetectionTest extends BaseTest {
      * Test for detecting copy-paste
      */
     public void testCopyPasteDetection() {
-        // Test data
         String filename = "file.txt";
-        VirtualFile file = myFixture.getTempDirFixture().createFile(filename);
         String content = "A small string";
-        Transferable transferableContent = new SimpleTransferable(content,
-                DataFlavor.stringFlavor);
+        VirtualFile file = myFixture.getTempDirFixture().createFile(filename);
+
+        // Setup the test file
+        myFixture.configureByFile(filename);
 
         // Set the clipboard content
-        CopyPasteManager.getInstance().setContents(transferableContent);
+        CopyPasteManager.getInstance().setContents(new SimpleTransferable(
+                content, DataFlavor.stringFlavor));
 
-        myFixture.configureByFile(filename);
         // Paste contents
         myFixture.performEditorAction(IdeActions.ACTION_EDITOR_PASTE);
 
         // Check the tracked changes
         assertChangeListSize(file.getPath(), 1);
-        assertOneChangeMatches(file.getPath(), (c) -> c.source == Change.Source.CLIPBOARD
-                && Objects.equals(c.oldString, "")
-                && Objects.equals(c.newString, content));
+        assertOneChangeMatches(file.getPath(), (c) ->
+                c.source == Change.Source.CLIPBOARD
+                        && Objects.equals(c.oldString, "")
+                        && Objects.equals(c.newString, content));
     }
 }
