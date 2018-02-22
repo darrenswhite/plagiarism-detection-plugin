18532428b2bb3a3aa731d97a74f321cb5fe1cd92
Darren White <darrenswhite95@gmail.com>
2018-02-22 17:33:16 +0000

    Added submit functionality server-side
    Currently this will just save the submitted XML file. Only XML files
    are accepted.

--- server/server/dashboard/views.py
+++ server/server/dashboard/views.py
@@ -1,7 +1,9 @@
 import logging
+import os
 
-from flask import Blueprint, abort, render_template, request
+from flask import Blueprint, abort, render_template, request, flash, redirect
 from flask_login import login_required, current_user
+from werkzeug.utils import secure_filename
 
 from server import app
 
@@ -9,6 +11,8 @@ from server import app
 dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 log = logging.getLogger(__name__)
+
+ALLOWED_EXTENSIONS = ['xml']
 
 
 @app.errorhandler(403)
@@ -35,9 +39,34 @@ def overview():
 def submit():
     if current_user.is_staff():
         abort(403, 'Staff members cannot post submissions.')
-    else:
-        # Try and submit the POST form submission
-        if request.method == 'POST':
-            log.debug(request.files)
-            log.debug(request.form)
-        return render_template('dashboard/submit.html')
+
+    # Try and submit the POST form submission
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part.', 'danger')
+            return redirect(request.url)
+
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No file submitted.', 'danger')
+            return redirect(request.url)
+
+        # Check file extensions
+        if file and valid_filename(file.filename):
+            # Check filename before saving it
+            filename = secure_filename(file.filename)
+            # TODO Upload file to MongoDB - for now just save the file
+            file.save(os.path.join('/plagiarism_detection/server', filename))
+            flash('Submission file saved successfully.', 'success')
+        else:
+            flash('Invalid file. File type must be xml.', 'danger')
+
+    return render_template('dashboard/submit.html')
+
+
+def valid_filename(filename):
+    return '.' in filename and filename.rsplit('.', 1)[
+        1].lower() in ALLOWED_EXTENSIONS
