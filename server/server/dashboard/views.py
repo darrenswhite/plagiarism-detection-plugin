ffc4580be04aec3d2ab5c4c8c5e3db4aa64d6ade
Darren White <darrenswhite95@gmail.com>
2018-02-22 16:39:35 +0000

    Added 403 for staff submit

--- server/server/dashboard/views.py
+++ server/server/dashboard/views.py
@@ -1,8 +1,21 @@
-from flask import Blueprint, render_template
+import logging
+
+from flask import Blueprint, abort, render_template, request
 from flask_login import login_required, current_user
+
+from server import app
 
 # The dashboard blueprint, the index will be at /dashboard
 dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
+
+log = logging.getLogger(__name__)
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    log.error(error)
+    return render_template('dashboard/403.html',
+                           description=error.description), 403
 
 
 @dashboard.route('/')
@@ -21,6 +34,9 @@ def overview():
 @login_required
 def submit():
     if current_user.is_staff():
-        pass
+        abort(403, 'Staff members cannot post submissions.')
     else:
+        # Try and submit the POST form submission
+        if request.method == 'POST':
+            log.debug(request.form)
         return render_template('dashboard/submit.html')
