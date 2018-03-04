b393d863cf15dcd8a747485530ff4c63cf91efeb
Darren White <darrenswhite95@gmail.com>
2018-03-04 14:35:02 +0000

    Added existing user signin test

--- server/tests/test_signin.py
+++ server/tests/test_signin.py
@@ -7,18 +7,20 @@ from tests.base import BaseTest
 
 
 class TestSignin(BaseTest):
+    def search(*args, **kwargs):
+        if 'BASE' not in args \
+                and 'attributes' in kwargs \
+                and 'gecos' in kwargs['attributes']:
+            return True
+        else:
+            return False
+
     @patch('ldap3.core.connection.Connection.entries')
     @patch('ldap3.core.connection.Connection.search')
     @patch('ldap3.core.connection.Connection.bind')
     def test_first_time_signin(self, mock_bind, mock_search, mock_entries):
-        def search(*args, **kwargs):
-            if 'BASE' in args:
-                return False
-            else:
-                return True
-
         mock_bind.return_value = True
-        mock_search.side_effect = search
+        mock_search.side_effect = self.search
         mock_entries.__getitem__.return_value.__getitem__.return_value.value = \
             'John Smith,ADN,,,[ABUG]'
 
@@ -44,6 +46,48 @@ class TestSignin(BaseTest):
         self.assertEqual('jos1', doc['uid'])
         self.assertEqual('ABUG', doc['user_type'])
         self.assertEqual(0, len(doc['submissions']))
+
+        response = future()
+
+        self.assertEqual(response.status_code, 302)
+        with server.app.test_request_context():
+            self.assertEqual(response.location,
+                             url_for('dashboard.overview', _external=True))
+
+    @patch('ldap3.core.connection.Connection.entries')
+    @patch('ldap3.core.connection.Connection.search')
+    @patch('ldap3.core.connection.Connection.bind')
+    def test_existing_signin(self, mock_bind, mock_search, mock_entries):
+        mock_bind.return_value = True
+        mock_search.side_effect = self.search
+        mock_entries.__getitem__.return_value.__getitem__.return_value.value = \
+            'John Smith,ADN,,,[ABUG]'
+
+        future = go(self.app.post, '/', data={
+            'uid': 'jos1',
+            'password': 'abc123',
+        })
+
+        request = self.mockdb.receives(
+            Command('find', 'submissions', filter={'uid': 'jos1'}))
+        request.ok(cursor={'id': 0, 'firstBatch': [{'uid': 'jos1'}]})
+        request = self.mockdb.receives(
+            Command('update', 'submissions', updates=[{
+                'q': {
+                    'uid': 'jos1'
+                },
+                'u': {
+                    '$set': {
+                        'submissions': [],
+                        'full_name': 'John Smith',
+                        'uid': 'jos1',
+                        'user_type': 'ABUG'
+                    }
+                },
+                'multi': False,
+                'upsert': False
+            }]))
+        request.ok()
 
         response = future()
 
