2ccaf6e509ae9f970a5c7945a41834df1d4f92ec
Darren White <darrenswhite95@gmail.com>
2018-04-04 19:42:53 +0100

    Add basic Pygal chart
    Don't show Submit button on staff dashboard

--- server/server/dashboard/views.py
+++ server/server/dashboard/views.py
@@ -1,5 +1,6 @@
 import logging
 
+import pygal
 from flask import Blueprint, abort, render_template, request, flash, redirect
 from flask_login import login_required, current_user
 
@@ -15,6 +16,9 @@ log = logging.getLogger(__name__)
 
 # Only allow uploading on XML files
 ALLOWED_EXTENSIONS = ['xml']
+
+# Source values for charts
+_SOURCES = ['CLIPBOARD', 'EXTERNAL', 'OTHER']
 
 
 @dashboard.errorhandler(403)
@@ -36,33 +40,71 @@ def overview():
     """
     # Show staff or student dashboard depending on user
     if current_user.is_staff():
-        # Find all users' submissions
-        all_user_data = list(server.submissions.find())
-        all_user_submissions = []
-
-        # Add name to each submission
-        for user in all_user_data:
-            submissions = user['submissions']
-            for s in submissions:
-                s['full_name'] = user['full_name']
-            all_user_submissions.append(submissions)
-
-        squashed_submissions = []
-
-        # Squash all submissions into a single list
-        for user_submissions in all_user_submissions:
-            for s in user_submissions:
-                squashed_submissions.append(s)
-
-        return render_template('dashboard/staff.html',
-                               submissions=squashed_submissions)
+        return overview_staff()
     else:
-        # Find all of the current users' submissions
-        user_data = server.submissions.find(
-            {'uid': current_user.uid}).next()
-        user_submissions = user_data['submissions'] if user_data else []
-        return render_template('dashboard/student.html',
-                               submissions=user_submissions)
+        return overview_student()
+
+
+def overview_staff():
+    # Find all users' submissions
+    all_user_data = list(server.submissions.find())
+    all_user_submissions = []
+
+    # Add name to each submission
+    for user in all_user_data:
+        submissions = user['submissions']
+        for s in submissions:
+            s['full_name'] = user['full_name']
+
+            if 'result' in s:
+                merged_fts_data = []
+
+                # Merge all fts data from each file to make one data set
+                for p, r in s['result'].items():
+                    if 'frequency_time_source_data' in r:
+                        merged_fts_data += r['frequency_time_source_data']
+
+                # Sort fts data by time
+                merged_fts_data = sorted(merged_fts_data,
+                                         key=lambda d: int(d['t']))
+
+                # Create a scatter chart for the data
+                scatter_chart = pygal.XY(disable_xml_declaration=True)
+                scatter_chart.title = 'Character Frequency vs. Time'
+                scatter_chart.x_title = 'Time (ms)'
+                scatter_chart.y_title = 'Frequency'
+
+                # Plot all data as a line
+                data = [(r['t'], r['f']) for r in merged_fts_data]
+                scatter_chart.add('', data, show_dots=False)
+
+                # Plot each source as a scatter plot using its color
+                for source in _SOURCES:
+                    data = [(r['t'], r['f']) for r in merged_fts_data if
+                            r['s'] == source]
+                    scatter_chart.add(source, data, stroke=False)
+
+                s['scatter_chart'] = scatter_chart
+        all_user_submissions.append(submissions)
+
+    squashed_submissions = []
+
+    # Squash all submissions into a single list
+    for user_submissions in all_user_submissions:
+        for s in user_submissions:
+            squashed_submissions.append(s)
+
+    return render_template('dashboard/staff.html',
+                           submissions=squashed_submissions)
+
+
+def overview_student():
+    # Find all of the current users' submissions
+    user_data = server.submissions.find(
+        {'uid': current_user.uid}).next()
+    user_submissions = user_data['submissions'] if user_data else []
+    return render_template('dashboard/student.html',
+                           submissions=user_submissions)
 
 
 @dashboard.route('/submit', methods=['GET', 'POST'])
